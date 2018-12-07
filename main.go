3c783cf16e6eb1aa29c6e9d793e52d95b4bf5514
weineel <LiJF_wn@163.com>
2018-12-07 11:26:35 +0800

    feat(go webassembly): go 编写 webassembly 实践

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/weineel/go-sprint/kit"
-)
-
 func main() {
-	fmt.Println("main")
+	println("main")
 	s := []int{23, 44, 5, 7}
-	fmt.Println(kit.ReverseInt(s))
+	println(s[2])
 }
