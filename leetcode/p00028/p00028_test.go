9b16acd4fddf38cc2bad305e0ed855d127a35eed
weineel <LiJF_wn@163.com>
2018-12-07 17:59:39 +0800

    feat(Implement strStr): https://leetcode.com/problems/implement-strstr/

--- leetcode/p00028/p00028_test.go
+++ leetcode/p00028/p00028_test.go
@@ -8,7 +8,19 @@ import (
 )
 
 var _ = Describe("P00028", func() {
-	It("input: '', expect: 0", func() {
+	It("input: 'hello', '', expect: 0", func() {
 		Expect(StrStr("hello", "")).To(Equal(0))
+	})
+
+	It("input: 'hello', 'hello', expect: 0", func() {
+		Expect(StrStr("hello", "hello")).To(Equal(0))
+	})
+
+	It("input: 'hello', 'll', expect: 2", func() {
+		Expect(StrStr("hello", "ll")).To(Equal(2))
+	})
+
+	It("input: 'weineel', 'nee', expect: 3", func() {
+		Expect(StrStr("weineel", "nee")).To(Equal(3))
 	})
 })
