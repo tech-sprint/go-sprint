73b365fc8b15da8084268663791a57068255470b
weineel <LiJF_wn@163.com>
2019-01-17 15:10:24 +0800

    feat(Search in Rotated Sorted Array): 使用查表法优化代码

--- leetcode/p00033/p00033.go
+++ leetcode/p00033/p00033.go
@@ -7,34 +7,29 @@ func SearchInRotatedSortedArray(nums []int, target int) int {
 	}
 	start, end := 0, len(nums)-1
 	firstValue := nums[0]
-	// 目标值是否在左半部分
-	inLeft := target >= firstValue
-
+	table := [8]bool{false, true, false, false, true, true, false, true}
 	for start <= end {
 		mid := (start + end) / 2
 		if nums[mid] == target {
 			return mid
 		}
-		if inLeft {
-			if nums[mid] >= firstValue {
-				if nums[mid] > target {
-					end = mid - 1
-				} else if nums[mid] < target {
-					start = mid + 1
-				}
-			} else {
-				end = mid - 1
-			}
+		var tp, mp, mt int
+		if target < firstValue {
+			// 目标值在右半部分
+			tp = 1 << 2
+		}
+		if nums[mid] < firstValue {
+			// 中间值在右半部分
+			mp = 1 << 1
+		}
+		if target > nums[mid] {
+			// 目标值大于中间值
+			mt = 1
+		}
+		if table[tp+mp+mt] {
+			start = mid + 1
 		} else {
-			if nums[mid] >= firstValue {
-				start = mid + 1
-			} else {
-				if nums[mid] > target {
-					end = mid - 1
-				} else if nums[mid] < target {
-					start = mid + 1
-				}
-			}
+			end = mid - 1
 		}
 	}
 	return -1
